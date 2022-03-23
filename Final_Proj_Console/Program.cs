2e41bc417026655f2c2f7fa71c36c4dda82dc057
ParkerS00 <parkerswenson00@gmail.com>
2022-03-22 21:44:52 -0600

    added code main method won't work

---
+++ Final_Proj_Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Final_Proj_Lib;
+using Final_Proj_Main;
+
+namespace Final_Proj_Console
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeworkTracker.createAccount();
+        }
+    }
+}
