e8b5393ef03f6039122c485b490c5bb45f92750f
ParkerS00 <parkerswenson00@gmail.com>
2022-04-13 11:00:27 -0600

    log ins and out working starting to do tests

--- Final_Proj_Tests/UnitTest1.cs
+++ Final_Proj_Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using CS_1410_Final_Proj_Lib;
 using NUnit.Framework;
 
@@ -9,13 +11,36 @@ public class Tests
     public void Setup()
     {
     }
-
+    
     [Test]
-    public void BuildAPerson()
+    public void PersonObjectContainsProperValues()
     {
         Person User = new Person("Parker", "Swenson", 4560, 1);
-        Person OtherUser = new Person("Parker", "Swenson", 4560, 1);
+        Assert.AreEqual("Parker", User.FirstName);
+        Assert.AreEqual("Swenson", User.LastName);
+        Assert.AreEqual(4560, User.Password);
+        Assert.AreEqual(1, User.Id);
+    }
 
-        Assert.AreEqual(OtherUser, User);
+    [Test]
+    public void HomeworkObjectContainsProperValues()
+    {
+        DateTime dateAdded = new DateTime(2022, 4, 10);
+        DateTime dateDue = new DateTime(2022, 4, 13);
+        Homework assignment = new Homework("Checkin 4", dateAdded, dateDue, 20);
+        Assert.AreEqual("Checkin 4", assignment.AssignmentName);
+        Assert.AreEqual(dateAdded, assignment.DateAdded);
+        Assert.AreEqual(dateDue, assignment.DueDate);
+        Assert.AreEqual(20, assignment.PointsWorth);
+    }
+
+    [Test]
+    public void AddItemToList()
+    {
+        DateTime dateAdded = new DateTime(2022, 4, 10);
+        DateTime dateDue = new DateTime(2022, 4, 13);
+        Homework assignment = new Homework("Test", dateAdded, dateDue, 20);
+        List<Homework> homeworkList = new List<Homework>();
+        
     }
 }
